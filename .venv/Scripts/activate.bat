8010c0eb1caba8ff1cbef12673ec0e5bde2e7035
Kryhin Maksim <masian.grodno@gmail.com>
2024-07-15 17:41:53 +0300

    recreate venv anr reinstall request and aiogram

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Work\learning-py\Home work\_WeatherBot\.venv
+set VIRTUAL_ENV=E:\Work\WeatherBot\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
