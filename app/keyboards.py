8980ec0021c20995814f79d9ea9802919802dafb
Kryhin Maksim <masian.grodno@gmail.com>
2024-07-18 12:29:39 +0300

    remove /start buttion

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,8 +1,6 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-main = ReplyKeyboardMarkup(
-    keyboard=[[KeyboardButton(text="Узнать погоду")], [KeyboardButton(text="/start")]], resize_keyboard=True, input_field_placeholder="Давай узнаем погоду :)"
-)
+main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="Узнать погоду")]], resize_keyboard=True, input_field_placeholder="Давай узнаем погоду :)")
 
 chose_country = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text="Беларусь", callback_data="by")], [InlineKeyboardButton(text="Россия", callback_data="ru")]])
 
