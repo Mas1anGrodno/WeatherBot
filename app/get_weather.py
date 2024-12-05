eebd49229dba9f13414e3d0129ee254789400a81
”Kryhin <kryhin.amzn@gmail.com>
2024-12-05 14:10:52 +0300

    v 1.0

--- app/get_weather.py
+++ app/get_weather.py
@@ -1,75 +1,97 @@
+import requests
+
 from deep_translator import GoogleTranslator
 from datetime import datetime, timezone
-from weather_requests import get_city_coord, weather_request, weather_request_overview
+
+from .weather_requests import get_weather_forecast, weather_request, get_weather_forecast_overview
 
 
 def convert_unix_timestamp(unix_timestamp: int) -> str:
-    weather_data = weather_request(coords)
-    offset = weather_data["timezone_offset"]
-    unix_timestamp += offset
+    unix_timestamp += 10800
     return datetime.fromtimestamp(unix_timestamp, tz=timezone.utc).strftime("%H:%M:")
 
 
-coords = get_city_coord("grodno", "by")
+# coords = get_city_coord("grodno", "by")
 
 
-def weather_now():
-    weather_data = weather_request(coords)
+def weather_now(city_name: str, country_name: str) -> dict:
+    try:
+        weather_data = get_weather_forecast(city_name, country_name)
+        if not weather_data:
+            raise ValueError("Данные не получены")
 
-    current_time = convert_unix_timestamp(weather_data["current"]["dt"])
-    current_temp = weather_data["current"]["temp"]
-    feels_like = weather_data["current"]["feels_like"]
-    current_pressure = weather_data["current"]["pressure"]
-    current_humidity = weather_data["current"]["humidity"]
-    weather_description = weather_data["current"]["weather"][0]["description"]
+        print("Данные получены")
+        current_time = convert_unix_timestamp(weather_data["current"]["dt"])
+        weather_description = weather_data["current"]["weather"][0]["description"]
 
-    return f"""
-    Сейчас - {current_time}
-    В выбранной локации сейчас - {weather_description}
-    Температура воздуха - {current_temp} °C
-    Ощущается как - {feels_like} °C
-    Давление - {current_pressure} гПа
-    Влажность - {current_humidity} %
-    """
+        current_temp = round(weather_data["current"]["temp"])
+        feels_like_temp = round(weather_data["current"]["feels_like"])
+        current_pressure = weather_data["current"]["pressure"]
+        current_humidity = weather_data["current"]["humidity"]
 
+        weather_alerts_start = convert_unix_timestamp(weather_data["alerts"][0]["start"])
+        weather_alerts_end = convert_unix_timestamp(weather_data["alerts"][0]["end"])
+        weather_alerts = GoogleTranslator(source="en", target="ru").translate(weather_data["alerts"][0]["event"])
 
-# print(weather_now())
-
-
-def weather_now_plus_three_hours():
-    weather_data = weather_request(coords)
-    current_time = convert_unix_timestamp(weather_data["current"]["dt"])
-    current_temp = weather_data["current"]["temp"]
-    weather_description = weather_data["current"]["weather"][0]["description"]
-
-    time_plus_3 = convert_unix_timestamp(weather_data["hourly"][3]["dt"])
-    time_plus_3_temp = weather_data["hourly"][3]["temp"]
-    time_plus_3_weather_description = weather_data["hourly"][3]["weather"][0]["description"]
-
-    time_plus_6 = convert_unix_timestamp(weather_data["hourly"][6]["dt"])
-    time_plus_6_temp = weather_data["hourly"][6]["temp"]
-    time_plus_6_weather_description = weather_data["hourly"][6]["weather"][0]["description"]
-
-    time_plus_9 = convert_unix_timestamp(weather_data["hourly"][9]["dt"])
-    time_plus_9_temp = weather_data["hourly"][9]["temp"]
-    time_plus_9_weather_description = weather_data["hourly"][9]["weather"][0]["description"]
-
-    return f"""
-    Сейчас - {current_time}
-    Температура воздуха {current_temp} °C и {weather_description}
-
-    в - {time_plus_3}
-    Температура воздуха {time_plus_3_temp} °C и {time_plus_3_weather_description}
-
-    в - {time_plus_6}
-    Температура воздуха {time_plus_6_temp} °C и {time_plus_6_weather_description}
-
-    в - {time_plus_9}
-    Температура воздуха {current_temp} °C и {weather_description}
-    """
+        return f"""
+Сейчас                              {current_time}                   
+В выбранной локации сейчас          {weather_description}            
+Температура воздуха                 {current_temp} °C                
+Ощущается как                       {feels_like_temp} °C             
+Давление                            {current_pressure} гПа           
+Влажность                           {current_humidity} %             
+C {weather_alerts_start} до {weather_alerts_end} возможны(ен) {weather_alerts} 
+        """
+    except (requests.RequestException, ValueError) as e:
+        return f"Ошибка: {e}"
 
 
-# print(weather_now_plus_three_hours())
+# print(weather_now("grodno", "by"))
+
+
+def weather_now_plus_three_hours(city_name: str, country_name: str) -> dict:
+
+    try:
+        weather_data = get_weather_forecast(city_name, country_name)
+        if not weather_data:
+            raise ValueError("Данные не получены")
+
+        print("Данные получены")
+
+        current_time = convert_unix_timestamp(weather_data["current"]["dt"])
+        current_temp = round(weather_data["current"]["temp"])
+        weather_description = weather_data["current"]["weather"][0]["description"]
+
+        time_plus_3 = convert_unix_timestamp(weather_data["hourly"][3]["dt"])
+        time_plus_3_temp = round(weather_data["hourly"][3]["temp"])
+        time_plus_3_weather_description = weather_data["hourly"][3]["weather"][0]["description"]
+
+        time_plus_6 = convert_unix_timestamp(weather_data["hourly"][6]["dt"])
+        time_plus_6_temp = round(weather_data["hourly"][6]["temp"])
+        time_plus_6_weather_description = weather_data["hourly"][6]["weather"][0]["description"]
+
+        time_plus_9 = convert_unix_timestamp(weather_data["hourly"][9]["dt"])
+        time_plus_9_temp = round(weather_data["hourly"][9]["temp"])
+        time_plus_9_weather_description = weather_data["hourly"][9]["weather"][0]["description"]
+
+        return f"""
+        Сейчас - {current_time}
+        Температура воздуха {current_temp} °C и {weather_description}
+
+        в - {time_plus_3}
+        Температура воздуха {time_plus_3_temp} °C и {time_plus_3_weather_description}
+
+        в - {time_plus_6}
+        Температура воздуха {time_plus_6_temp} °C и {time_plus_6_weather_description}
+
+        в - {time_plus_9}
+        Температура воздуха {time_plus_9_temp} °C и {time_plus_9_weather_description}
+        """
+    except (requests.RequestException, ValueError) as e:
+        return f"Ошибка: {e}"
+
+
+# print(weather_now_plus_three_hours("grodno", "by"))
 
 
 def weather_today():
@@ -91,7 +113,7 @@ def weather_today():
     """
 
 
-print(weather_today())
+# print(weather_today())
 
 # def weather_three_days():
 #     weather_data = weather_request(coords)
@@ -126,11 +148,16 @@ print(weather_today())
 # print(weather_three_days())
 
 
-def overview():
-    weather_data_overview = weather_request_overview(coords)
+def weather_overview(city_name: str, country_name: str) -> dict:
+    try:
+        weather_data_overview = get_weather_forecast_overview(city_name, country_name)
+        if not weather_data_overview:
+            raise ValueError("Данные описания погодных условий не получены")
 
-    translated_overview = GoogleTranslator(source="en", target="ru").translate(weather_data_overview["weather_overview"])
-    print(translated_overview)
+        translated_overview = GoogleTranslator(source="en", target="ru").translate(weather_data_overview["weather_overview"])
+        return translated_overview
+    except (requests.RequestException, ValueError) as e:
+        return f"Ошибка: {e}"
 
 
-# overview()
+# print(weather_overview("grodno", "by"))
--- app/weather_requests.py
+++ app/weather_requests.py
@@ -11,22 +11,73 @@ OW_API_KEY = env("OW_API_KEY")
 
 def get_city_coord(city_name: str, country_name: str) -> Tuple[float, float]:
     coord_by_name = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name},{country_name}&limit=1&appid={OW_API_KEY}"
-    get_coord = requests.get(coord_by_name)
-    coord = get_coord.json()
-    return float(coord[0]["lat"]), float(coord[0]["lon"])
+    try:
+        get_coord = requests.get(coord_by_name)
+        get_coord.raise_for_status()  # Проверка, что запрос выполнен успешно
+        coord = get_coord.json()
+        if not coord:
+            raise ValueError("Данные координат не получены")
+        return float(coord[0]["lat"]), float(coord[0]["lon"])
+    except (requests.RequestException, ValueError) as e:
+        print(f"Ошибка: {e}")
+        return None, None
 
 
 def weather_request(coords: Tuple[float, float]) -> dict:
     lat, lon = coords
     weather_request = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=minutely&units=metric&lang=ru&appid={OW_API_KEY}"
-    r = requests.get(weather_request)
-    response = r.json()
-    return response
+    try:
+        r = requests.get(weather_request)
+        r.raise_for_status()  # Проверка, что запрос выполнен успешно
+        response = r.json()
+        if not response:
+            raise ValueError("Данные о погоде не получены")
+        return response
+    except (requests.RequestException, ValueError) as e:
+        print(f"Ошибка: {e}")
+        return {}
 
 
 def weather_request_overview(coords: Tuple[float, float]) -> dict:
     lat, lon = coords
     weather_request_overview = f"https://api.openweathermap.org/data/3.0/onecall/overview?lat={lat}&lon={lon}&units=metric&lang=ru&appid={OW_API_KEY}"
-    r = requests.get(weather_request_overview)
-    response = r.json()
-    return response
+    try:
+        r = requests.get(weather_request_overview)
+        r.raise_for_status()  # Проверка, что запрос выполнен успешно
+        response = r.json()
+        if not response:
+            raise ValueError("Данные описаня погодных условий не получены")
+        return response
+    except (requests.RequestException, ValueError) as e:
+        print(f"Ошибка: {e}")
+        return {}
+
+
+def get_weather_forecast(city_name: str, country_name: str) -> dict:
+    # Сначала получаем координаты города
+    coords = get_city_coord(city_name, country_name)
+
+    # Проверяем, что координаты получены успешно
+    if coords == (None, None):
+        print("Ошибка: не удалось получить координаты города.")
+        return {}
+
+    # Затем получаем данные прогноза погоды по координатам
+    weather_data = weather_request(coords)
+
+    return weather_data
+
+
+def get_weather_forecast_overview(city_name: str, country_name: str) -> dict:
+    # Сначала получаем координаты города
+    coords = get_city_coord(city_name, country_name)
+
+    # Проверяем, что координаты получены успешно
+    if coords == (None, None):
+        print("Ошибка: не удалось получить координаты города.")
+        return {}
+
+    # Затем получаем данные прогноза погоды по координатам
+    weather_data = weather_request_overview(coords)
+
+    return weather_data
